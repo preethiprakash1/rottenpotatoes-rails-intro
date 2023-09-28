d001af97e2c72e9fca48fdea69a7d4558d9cd29c
pp2769 Prakash <preethi.prakash@columbia.edu>
2023-09-28 01:44:23 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+  end
+
+  def self.with_ratings(ratings_list)
+    if ratings_list.nil? || ratings_list.empty?
+      return Movie.all
+    else
+      return Movie.where(rating: ratings_list)
+    end
+  end
+
 end
